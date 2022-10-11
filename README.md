91ce22677a9f5088f3cc8fbeb6a405a2d227738e
shaikshan <shaikroshan1997@gmail.com>
2022-10-11 18:17:30 +0530

    added docker file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # This is the testing of git.
 # This is the dev branch.
+```
+docker build -t custom_app_name
+```
--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Hello")
+
+from flask import Flask,request,jsonify
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def home():
+    return "Hello World"
+
+if __name__=="__main__":
+    app.run(port=5000,host='0.0.0.0')
